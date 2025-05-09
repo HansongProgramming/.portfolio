0faa0d632db7cd895e2a2d2333f390feffab993e
Hans Harold Lee Flores <125562851+HansongProgramming@users.noreply.github.com>
2025-05-09 20:49:04 +0800

    finalization first pass

--- contentHandler.js
+++ contentHandler.js
@@ -149,13 +149,3 @@ document.addEventListener('DOMContentLoaded', function () {
     }, { once: true });
   });
   
-  
-
-function openLightbox(src) {
-    document.getElementById('lightbox-img').src = src;
-    document.getElementById('lightbox').classList.add('active');
-  }
-
-  function closeLightbox() {
-    document.getElementById('lightbox').classList.remove('active');
-  }
